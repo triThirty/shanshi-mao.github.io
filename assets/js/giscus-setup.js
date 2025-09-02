d9889adfebef207b0699e1da530e6e60a6056aaa
triThirty <maoshanshi@outlook.com>
2025-09-02 06:19:00 +0000

    Deploying to gh-pages from @ triThirty/triThirty.github.io@6d19595dc82ae0c22313831c54f2c4c2bcd06ba2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "triThirty/shanshi-mao.github.io",
+    "data-repo": "triThirty/triThirty.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
